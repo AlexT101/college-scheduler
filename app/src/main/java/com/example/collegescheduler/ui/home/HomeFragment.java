8fdf5436c0df00055e075f0f2d4c58284ec1e25d
Milly Zheng <millyzheng57@gmail.com>
2024-01-27 13:37:30 -0500

    Fixed changing screens

--- app/src/main/java/com/example/collegescheduler/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/collegescheduler/ui/home/HomeFragment.java
@@ -7,14 +7,28 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.collegescheduler.ClassCard;
+import com.example.collegescheduler.ClassCardAdapter;
+import com.example.collegescheduler.R;
+import com.example.collegescheduler.SpacesItemDecoration;
 import com.example.collegescheduler.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    private RecyclerView recyclerView;
+    private ClassCardAdapter adapter;
+    private List<ClassCard> classCardList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +41,28 @@ public class HomeFragment extends Fragment {
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstance) {
+
+        view = getView();
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+    //findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.recycler_view_spacing);
+        recyclerView.addItemDecoration(new SpacesItemDecoration(spacingInPixels));
+
+        classCardList = new ArrayList<>();
+        classCardList.add(new ClassCard("CS2340", "TR: 2:00-3:15", "IC103"));
+        classCardList.add(new ClassCard("CS1332", "MWF: 2:00-2:50", "CULC144"));
+        classCardList.add(new ClassCard("CS1332", "MWF: 2:00-2:50", "CULC144"));
+        classCardList.add(new ClassCard("CS1332", "MWF: 2:00-2:50", "CULC144"));
+
+        adapter = new ClassCardAdapter(classCardList);
+        recyclerView.setAdapter(adapter);
+
     }
 
     @Override
