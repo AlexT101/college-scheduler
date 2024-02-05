6fd11626091af898bf3eb64f08690723f74f8b90
AlexT <alex.thummas66@gmail.com>
2024-02-04 23:45:10 -0500

    Added intro screen, updated colors

---
+++ app/src/main/java/com/example/collegescheduler/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.example.collegescheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        Button getStartedButton = findViewById(R.id.buttonGetStarted);
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Close this activity
+            }
+        });
+    }
+}
